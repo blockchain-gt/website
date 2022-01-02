26ec5a2f2359ec60d555705a129dc13b4f4844bc
Patrick Hultquist <patrick@patrick.today>
2022-01-01 23:56:14 -0600

    add interactive sidebar

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -3,14 +3,25 @@
 	export let sections: Result<SidebarSection>[];
 	import PrismicDOM from 'prismic-dom';
 	import linkResolver from './prismic/linkResolver';
+
+	// Odd workaround because $page doesn't contain a `path`, only `url`
+	$: w = typeof window === 'undefined' ? null : window;
 </script>
 
 <div class="w-full p-8 pt-16 m-4">
-	<h4 class="font-bold mb-4">Blockchain @ GT</h4>
+	<h4 class="font-bold mb-4"><a href="/">Blockchain @ GT</a></h4>
+	<!-- <pre>
+		{JSON.stringify($page, null, 2)}
+	</pre> -->
 	{#each sections.sort((a, b) => a.data.order - b.data.order) as section}
 		<div class="mb-6">
 			{#each section.data.links as link}
-				<div class="my-2">
+				<div
+					class="my-2 p-1.5 px-2.5 rounded-sm {w &&
+					PrismicDOM.Link.url(link.url, linkResolver) === w.location.pathname
+						? 'bg-gray-100'
+						: ''}"
+				>
 					<a href={PrismicDOM.Link.url(link.url, linkResolver)}>
 						{link.link_title}
 					</a>
