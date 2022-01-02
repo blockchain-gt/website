cc83274f0a2c91c04a4151dae4ab9b82881f5129
Patrick Hultquist <patrick@patrick.today>
2022-01-02 12:13:07 -0600

    events page

--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -1,3 +1,41 @@
+<script context="module" lang="ts">
+	import EventPreview from '$lib/EventPreview.svelte';
+
+	import Client from '$lib/prismic/client';
+	import linkResolver from '$lib/prismic/linkResolver';
+	import type { Result } from '$lib/types/prismic';
+	import PrismicDOM from 'prismic-dom';
+
+	export const load = async () => {
+		const page = await Client.getSingle('events_home', {});
+
+		return {
+			props: {
+				pageResult: page
+			}
+		};
+	};
+</script>
+
+<script lang="ts">
+	export let pageResult: Result<any>;
+	let page = pageResult.data;
+</script>
+
 <div>
-	<h1 class="page-title">Events</h1>
+	<h1 class="page-title">{PrismicDOM.RichText.asText(page.title)}</h1>
+	<div class="prose my-8">
+		{@html PrismicDOM.RichText.asHtml(page.body)}
+	</div>
+	<div class="upcoming-events mt-2 mb-6 ">
+		<h2 class="page-subtitle">Upcoming Events</h2>
+		<div class="flex flex-row flex-wrap space-x-8">
+			<EventPreview />
+			<EventPreview />
+			<EventPreview />
+		</div>
+	</div>
+	<div class="past-events mt-2 mb-6">
+		<h2 class="page-subtitle">Past Events</h2>
+	</div>
 </div>
